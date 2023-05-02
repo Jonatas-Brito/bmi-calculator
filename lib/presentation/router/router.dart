0ceb3370b19c9f08e2d0148cf074526d1c07a10c
Jonatas Brito <jonatas.furtuna@gmail.com>
2023-05-02 16:45:18 -0300

    feat: implement route to HomePage

--- lib/presentation/router/router.dart
+++ lib/presentation/router/router.dart
@@ -1,5 +1,17 @@
+import 'package:calcule_imc/core/enums/route_enum.dart';
+import 'package:calcule_imc/presentation/home/pages/home_page.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 final GoRouter router = GoRouter(
-  routes: <GoRoute>[],
+  initialLocation: RouteEnum.home.path,
+  routes: <GoRoute>[
+    GoRoute(
+      path: RouteEnum.home.path,
+      name: RouteEnum.home.name,
+      builder: (_, GoRouterState state) => const HomePage(
+        key: ValueKey('home-page'),
+      ),
+    ),
+  ],
 );
