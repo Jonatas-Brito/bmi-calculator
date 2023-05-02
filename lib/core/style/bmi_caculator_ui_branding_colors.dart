42dab703405121bd3ce296ad1307f650218281ed
Jonatas Brito <jonatas.furtuna@gmail.com>
2023-05-02 11:36:04 -0300

    feat: add app branding colors

---
+++ lib/core/style/bmi_caculator_ui_branding_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BmiCalculatorUiBrandingColors {
+  /// Base to primary pure color
+  ///
+  static const Color _greenVogue = Color(0xFF023047);
+  static const Color _chatamsBlue = Color(0xFF175574);
+  static const Color _flushOrange = Color(0xFFFB8500);
+
+  static Color get primaryColor => _greenVogue;
+  static Color get secondaryColor => _chatamsBlue;
+  static Color get tertiaryColor => _flushOrange;
+}
