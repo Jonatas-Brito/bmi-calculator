b5038e3f3fd33ccb49148838d659f57b8ffd2623
Jonatas Brito <jonatas.furtuna@gmail.com>
2023-05-02 11:36:04 -0300

    feat: add app branding colors

--- lib/core/style/bmi_caculator_ui_branding_colors.dart
+++ lib/core/style/bmi_caculator_ui_branding_colors.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 class BmiCalculatorUiBrandingColors {
   /// Base to primary pure color
   ///
-  /// Figma token: $branding-color-primary-pure
-  static const Color greenVogue = Color(0xFF023047);
-  static const Color chatamsBlue = Color(0xFF175574);
-  static const Color flushOrange = Color(0xFFFB8500);
-  static const Color whitePure = Color(0xFFFFFFFF);
+  static const Color _greenVogue = Color(0xFF023047);
+  static const Color _chatamsBlue = Color(0xFF175574);
+  static const Color _flushOrange = Color(0xFFFB8500);
+
+  static Color get primaryColor => _greenVogue;
+  static Color get secondaryColor => _chatamsBlue;
+  static Color get tertiaryColor => _flushOrange;
 }
